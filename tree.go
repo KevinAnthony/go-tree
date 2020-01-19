95f15e1d88fd1d240ef14aef7a864dafa7ebf4ea
KevinAnthony <kevin.s.anthony@gmail.com>
2020-01-19 18:43:54 -0500

    turns out I hate writing documentation... who knew?

--- tree.go
+++ tree.go
@@ -1,3 +1,4 @@
+// Package provides generic implementation of trees with a way to get a specific implementation
 package tree
 
 import (
@@ -5,12 +6,16 @@ import (
 	"github.com/KevinAnthony/go-tree/types"
 )
 
+// this is used to ask for a specific type of tree
 type Type int
 
 const (
+	// used to ask for a Binary Search Tree from new
 	BinarySearchTree Type = 0
 )
 
+// Create a new tree, optional variadic data.
+// TODO once we have more then one implementation of Data, we should restrict all inserts to be the same type as root
 func New(t Type, data ...types.Data) types.Tree {
 	switch t {
 	case BinarySearchTree:
